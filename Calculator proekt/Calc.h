5197c737dbc9eef2e4a10b3d812ee6424d68c5c0
maksi <maksi@ADMIN>
2023-02-13 18:16:02 +0300

    Добавьте файлы проекта.

---
+++ Calculator proekt/Calc.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace Calculatorproekt {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Calc
+	/// </summary>
+	public ref class Calc : public System::Windows::Forms::Form
+	{
+	public:
+		Calc(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Calc()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Calc
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(488, 541);
+			this->Name = L"Calc";
+			this->Text = L"Calc";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
